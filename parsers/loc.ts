2b6717d26a83e326e2ed98e50779bf60e2dd8ee3
Leko <leko.noor@gmail.com>
2020-08-15 01:37:36 +0900

    Add pickLoC

--- parsers/loc.ts
+++ parsers/loc.ts
@@ -4,3 +4,14 @@ export const toLoC = ({ start, end }: { start: number; end: number }): LoC => ({
   start,
   length: end - start,
 })
+
+export const pickLoC = ({
+  start,
+  length,
+}: {
+  start: number
+  length: number
+}): LoC => ({
+  start,
+  length,
+})
