ca8144cbf4d32052e466324bc06edb0a6375a600
Leko <leko.noor@gmail.com>
2020-08-15 01:36:34 +0900

    lineEnding returns Str node

--- parsers/line-ending.ts
+++ parsers/line-ending.ts
@@ -1,5 +1,20 @@
 import { C_NEWLINE, C_CARRIAGE_RETURN } from '../scanner.ts'
-import { char, seq, option } from '../parser-combinator.ts'
+import { Str } from '../ast.ts'
+import { map, char, seq, option } from '../parser-combinator.ts'
+import { toLoC } from './loc.ts'
 
 // https://spec.commonmark.org/0.29/#line-ending
-export const lineEnding = seq(option(char(C_CARRIAGE_RETURN)), char(C_NEWLINE))
+export const lineEnding = map(
+  seq(option(char(C_CARRIAGE_RETURN)), char(C_NEWLINE)),
+  (r, end, start): Str => ({
+    type: 'str',
+    children: [
+      {
+        type: 'text',
+        text: r[1],
+        ...toLoC({ start, end }),
+      },
+    ],
+    ...toLoC({ start, end }),
+  })
+)
