2ae4c4899631a4fee2af416cfd658d4849643c33
Leko <leko.noor@gmail.com>
2020-08-15 11:26:12 +0900

    Refactor types.ts

--- ast/types.ts
+++ ast/types.ts
@@ -4,13 +4,15 @@ export type Position = [number, number]
 // [start, end]
 export type TextRange = [Position, Position]
 
-export type LoC = {
+export interface LoC {
   start: number
   length: number
 }
 
-export type ASTText<T> = T &
-  LoC & {
-    text: string
-  }
-export type ASTNode<T extends { children: any[] }> = T & LoC
+export interface ASTText extends LoC {
+  text: string
+}
+
+export interface ASTNode extends LoC {
+  children: never[]
+}
